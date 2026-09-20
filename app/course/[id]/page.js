7095ef75f9f9a9eca0a61c3babb5db2c74685c81
sayeed2758 <sayeedbackupmail@gmail.com>
2026-09-20 06:32:20 +0530

    Add files via upload

--- app/course/[id]/page.js
+++ app/course/[id]/page.js
@@ -1,16 +1,14 @@
 import { notFound } from "next/navigation";
-import { getCourseById } from "../../../lib/courses";
+import { courses, getCourseById } from "../../../lib/courses";
 import CourseDetailsClient from "../../../components/CourseDetailsClient";
 
 export function generateStaticParams() {
-  return ["1", "2", "3", "4", "5", "6"].map((id) => ({ id }));
+  return courses.map((course) => ({ id: course.id }));
 }
 
 export default async function CourseDetailsPage({ params }) {
   const { id } = await params;
   const course = getCourseById(id);
-
   if (!course) notFound();
-
   return <CourseDetailsClient course={course} />;
 }
